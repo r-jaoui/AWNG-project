4a93a702db109c01eabaeef82865d5717d5b42b7
Ruben JAOUI <rubenjaoui@outlook.fr>
2020-05-22 19:40:20 +0200

    Projet Personnel - Pages de Projets et de Taches d'un Projet

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('connect/', connect, name = "Connexion"),
     path('disconnect/', disconnect, name = "Deconnexion"),
+    path('projects/', projets, name = "Projets"),
+    path('projet/<int:id>', projet, name = "Projet"),
+    path('denied/', denied, name = "Refusé"),
 ]
--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import permission_required, login_required
 from .forms import *
+from .models import *
 
 # Create your views here.
 def connect(request):
@@ -12,6 +14,7 @@ def connect(request):
     if connected:
         name = request.user.first_name + " " + request.user.last_name
         username = request.user.username
+        return redirect(projets)
     else:
         if form.is_valid():
             error = True
@@ -20,7 +23,7 @@ def connect(request):
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
-                return redirect(connect)
+                return redirect(projets)
             else:
                 pass
     return render(request, 'taskmanager/connect.html', locals())
@@ -31,3 +34,56 @@ def disconnect(request):
         logout(request)
     return redirect(connect)
 
+def denied(request):
+    connected = request.user.is_authenticated
+    name = ""
+    username = ""
+    projets_list = None
+    if connected:
+        name = request.user.first_name + " " + request.user.last_name
+        username = request.user.username
+    return render(request, 'taskmanager/denied.html', locals())
+
+
+def projets(request):
+    connected = request.user.is_authenticated
+    name = ""
+    username = ""
+    projets_list = []
+    projets_taches_nbre = []
+    projets = []
+    if connected:
+        name = request.user.first_name + " " + request.user.last_name
+        username = request.user.username
+        if request.user.has_perm('taskmanager.view_projet'):
+            projets_list = list(Projet.objects.all())
+            for projet in projets_list:
+                projets_taches_nbre.append(0)
+                for tache in Tache.objects.all():
+                    if tache.projet == projet:
+                        projets_taches_nbre[-1]+=1
+                projets.append([projet, projets_taches_nbre[-1]])
+        else:
+            return redirect(denied)
+    else:
+        return redirect(connect)
+    return render(request, 'taskmanager/projects.html', locals())
+
+
+def projet(request, id):
+    connected = request.user.is_authenticated
+    name = ""
+    username = ""
+    projet = None
+    taches = None
+    if connected:
+        name = request.user.first_name + " " + request.user.last_name
+        username = request.user.username
+        if request.user.has_perm('taskmanager.view_projet') and request.user.has_perm('taskmanager.view_tache'):
+            projet = Projet.objects.get(id=id)
+            taches = Tache.objects.filter(projet = projet)
+        else:
+            return redirect(denied)
+    else:
+        return redirect(connect)
+    return render(request, 'taskmanager/projet.html', locals())
